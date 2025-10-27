dc0f7d6ce4e8427fbfaa4f4a1e541bd9ae66b024
unknown <funmilolaadebiyi52@gmail.com>
2025-10-27 15:19:33 +0100

    env-updates

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+console.log({ apiKey: import.meta.env.VITE_API_KEY})
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "get-expert-ffc09.firebaseapp.com",
